94f8a7bdac20b925857cfc1d7a42b9c8758e3297
AbhiswasPatil <47222009+AbhiswasPatil@users.noreply.github.com>
2021-06-11 20:34:19 +0530

    Update jsNew.js

--- jsNew.js
+++ jsNew.js
@@ -2,49 +2,73 @@ if (images.length === 0) {
   throw new Error("Array is empty!!");
 }
 
+function getItems() {
+  var items = document.getElementsByClassName("items")[0];
+  var itemsCollection = items.getElementsByClassName("item");
+  return itemsCollection;
+}
+
+function activateItem(item) {
+  item.className += " active";
+}
+
+function deactivateItem(activeItem) {
+  activeItem.className = activeItem.className.replace(" active", "");
+}
+
 // function to change the preview content based on current active class
 function changePreview(item) {
   var url = item.getElementsByTagName("img")[0].src;
   var title = item.getElementsByClassName("info")[0].textContent;
+
   var preview = document.getElementById("preview");
   preview.getElementsByTagName("img")[0].src = url;
-  document.getElementById("preview-footer").innerHTML = title;
+  preview.getElementsByClassName("info")[0].innerHTML = title;
+}
+
+// function to change set from one item to other and also update the preview
+function changeActiveState(currentActiveItem, toBeActiveItem) {
+  deactivateItem(currentActiveItem);
+  activateItem(toBeActiveItem);
+  changePreview(toBeActiveItem);
 }
 
 // function to go to next active class
-function nextActiveClass() {
-  var items = document.getElementsByClassName("item");
-  var activeClass = document.getElementsByClassName("active")[0];
-  var nextElement = activeClass.nextElementSibling;
+function setNextItemActive() {
+  var items = getItems();
+
+  var activeItem = document.getElementsByClassName("active")[0];
+  var nextElement = activeItem.nextElementSibling;
+
   if (!nextElement) {
     nextElement = items[0];
   }
-  activeClass.className = activeClass.className.replace(" active", "");
-  nextElement.className += " active";
-  changePreview(nextElement);
+
+  changeActiveState(activeItem, nextElement);
 }
 
 // function to go to previous active class
-function prevActiveClass() {
-  var items = document.getElementsByClassName("item");
-  var activeClass = document.getElementsByClassName("active")[0];
-  var prevElement = activeClass.previousElementSibling;
+function setPrevItemActive() {
+  var items = getItems();
+
+  var activeItem = document.getElementsByClassName("active")[0];
+  var prevElement = activeItem.previousElementSibling;
+
   if (!prevElement) {
     prevElement = items[items.length - 1];
   }
-  activeClass.className = activeClass.className.replace(" active", "");
-  prevElement.className += " active";
-  changePreview(prevElement);
+
+  changeActiveState(activeItem, prevElement);
 }
 
 // listens for keys movement UP and DOWN for changing active class
 document.onkeydown = function (e) {
   switch (e.key) {
     case "ArrowUp":
-      prevActiveClass();
+      setPrevItemActive();
       break;
     case "ArrowDown":
-      nextActiveClass();
+      setNextItemActive();
       break;
   }
 };
@@ -60,25 +84,18 @@ function createItemElement(item) {
   var infoDiv = document.createElement("div");
   infoDiv.className = "info";
   var infoText = document.createTextNode(item["title"]);
+
   infoDiv.appendChild(infoText);
   itemElement.appendChild(infoDiv);
 
   return itemElement;
 }
 
+//function which creates preview div
 function createPreview() {
-  var preview = document.createElement("div");
-  preview.setAttribute("id", "preview");
-
-  var imageForPreview = document.createElement("img"); //holds the preview image
-  imageForPreview.src = images[0]["previewImage"];
-  preview.appendChild(imageForPreview);
-
-  var preview_footer = document.createElement("p"); //holds the image title
-  preview_footer.setAttribute("id", "preview-footer");
-  preview_footer.appendChild(document.createTextNode(images[0]["title"]));
-  preview.appendChild(preview_footer);
-  return preview;
+  var createdNewElement = createItemElement(images[0]);
+  createdNewElement.setAttribute("id", "preview");
+  return createdNewElement;
 }
 
 function createItems() {
@@ -87,41 +104,31 @@ function createItems() {
 
   for (let indexInImages = 0; indexInImages < images.length; indexInImages++) {
     var item = createItemElement(images[indexInImages]); // item is element with className item
-    //console.log(images[indexInImages]);
     item.className = "item";
+
     if (indexInImages === 0) {
-      item.className += " active";
+      activateItem(item);
     }
-    //console.log(item)
+
     item.addEventListener("click", function () {
-      var activeClass = document.getElementsByClassName("active");
-      activeClass[0].className = activeClass[0].className.replace(
-        " active",
-        ""
-      );
-      this.className += " active";
-      changePreview(this);
+      var activeItem = document.getElementsByClassName("active")[0];
+      changeActiveState(activeItem, this);
     });
     items.appendChild(item);
   }
+
   return items;
 }
 
-function fillEverythingInMainContent(items, preview) {
-  // appended 2 divs to mainContent
-  var mainContent = document.getElementsByClassName("mainContent")[0];
-  mainContent.appendChild(items);
-  mainContent.appendChild(preview);
-}
-
-/// DOM building starts here
+// function which initialises evrything
 function buildPage() {
   //created preview part
   var previewElement = createPreview();
   //created items part
   var itemsElement = createItems();
-
-  fillEverythingInMainContent(itemsElement, previewElement);
+  var mainContent = document.getElementsByClassName("mainContent")[0];
+  mainContent.appendChild(itemsElement);
+  mainContent.appendChild(previewElement);
 }
 
 buildPage();
